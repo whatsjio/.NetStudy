a1920307e80eb6dee33bdb33a6f629da623d1f70
fcs <909412615@qq.com>
2020-09-28 23:00:19 +0800

    feature:管道初始代码

---
+++ repos/StreamTesttwo/管道测试发送端/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace 管道测试发送端
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadKey();
+        }
+    }
+}
