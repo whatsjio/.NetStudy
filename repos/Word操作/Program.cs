aef842f09414d5dabf862055cb443aa9190cdab1
changsheng fu <909412615@qq.com>
2021-08-13 15:30:29 +0800

    feature:添加word操作示例

---
+++ repos/Word操作/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aspose.Words;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Document doc = new Document("F:/Visual2019/ConsoleApp2/bin/Debug/股权收益转让合同（取值版）.docx");
+            doc.Range.Replace("ContractCode", "测试ContractCode");
+            doc.Save("股权收益转让合同（取值替换版）.docx");
+
+        }
+    }
+}
