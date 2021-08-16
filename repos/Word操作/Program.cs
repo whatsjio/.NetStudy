10d073637ad0b1c3f70f0d2e611a704ab3602227
changsheng fu <909412615@qq.com>
2021-08-16 09:59:19 +0800

    feature:破解版

--- repos/Word操作/Program.cs
+++ repos/Word操作/Program.cs
@@ -12,8 +12,9 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Document doc = new Document("F:/Visual2019/ConsoleApp2/bin/Debug/股权收益转让合同（取值版）.docx");
-            doc.Range.Replace("ContractCode", "测试ContractCode");
+            Document doc = new Document("F:/Visual2019/代码库/.NetStudy/repos/Word操作/bin/Debug/股权收益转让合同（取值版）.docx");
+
+            doc.Range.Replace("ContractCode", "测试ContractCode",false,false);
             doc.Save("股权收益转让合同（取值替换版）.docx");
 
         }
