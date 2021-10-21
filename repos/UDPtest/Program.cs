d2993bbf84fac8ecaaaf8d7f99a6cfa107ea0d70
changsheng fu <909412615@qq.com>
2021-10-21 16:01:54 +0800

    feature:UPD收发测试

--- repos/UDPtest/Program.cs
+++ repos/UDPtest/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace UDPtest
 {
@@ -10,7 +13,107 @@ namespace UDPtest
     {
         static void Main(string[] args)
         {
+            int port=0;
+            string hostname="";
+            string groupaddress="";
+            WriteLine("输入地址：");
+            groupaddress = ReadLine();
+            WriteLine("输入端口：");
+            var portstr = ReadLine();
+            int.TryParse(portstr, out port);
 
+            WriteLine("输入hostname：");
+            hostname = ReadLine();
+
+
+            IPEndPoint endpoint = 
+                GetIPEndPoint(port, hostname, false, groupaddress, false).Result;
+            Sender(endpoint, false, groupaddress).Wait();
+            ReadKey();
+
+        }
+
+
+        private static async Task Sender(IPEndPoint endPoint, bool broadcast, string groupAddress) {
+            try
+            {
+                string localhost = Dns.GetHostName();
+                using (var client=new UdpClient())
+                {
+                    var sendip = IPAddress.Parse(groupAddress);
+                    client.EnableBroadcast = broadcast;
+                    if (groupAddress != null) {
+                        client.JoinMulticastGroup(sendip);
+                    
+                    }
+                    bool completed = false;
+                    do
+                    {
+                        WriteLine("输入一个消息，或者退出");
+                        string input = ReadLine();
+                        WriteLine();
+                        completed = input.ToLower() == "bye";
+                        byte[] datagram = Encoding.UTF8.GetBytes($"{input}  from {localhost}");
+                        int sent = await client.SendAsync(datagram, datagram.Length, endPoint);
+                    } while (!completed);
+                    client.DropMulticastGroup(sendip);
+                }
+            }
+            catch (SocketException ex)
+            {
+                WriteLine(ex.Message);
+            }
+        
+        
+        }
+
+        /// <summary>
+        /// 获取ip终结点
+        /// </summary>
+        /// <param name="port"></param>
+        /// <param name="hostname"></param>
+        /// <param name="broadcast"></param>
+        /// <param name="groupaddress"></param>
+        /// <param name="ipv6"></param>
+        /// <returns></returns>
+        public static async Task<IPEndPoint> GetIPEndPoint(int port, string hostname, bool broadcast, string groupaddress, bool ipv6) {
+            IPEndPoint endPoint = null;
+            try
+            {
+                if (broadcast)
+                {
+                    endPoint = new IPEndPoint(IPAddress.Broadcast, port);
+                }
+                else if (!string.IsNullOrEmpty(hostname))
+                {
+                    IPHostEntry hostEntry = await Dns.GetHostEntryAsync(hostname);
+                    IPAddress address = null;
+                    if (ipv6)
+                    {
+                        address = hostEntry.AddressList.Where(t => t.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6).FirstOrDefault();
+                    }
+                    else
+                    {
+                        address = hostEntry.AddressList.Where(t => t.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).FirstOrDefault();
+                    }
+                    endPoint = new IPEndPoint(address, port);
+                }
+                else if (groupaddress != null)
+                {
+                    endPoint = new IPEndPoint(IPAddress.Parse(groupaddress), port);
+                }
+                else {
+                    throw new InvalidOperationException("参数不正确");
+                
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            return endPoint;
+        
         }
     }
 }
---
+++ repos/UdpReserver/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace UdpReserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string groupaddress = "";
+            int port = 0;
+            do
+            {
+                WriteLine("输入地址：");
+                groupaddress= ReadLine();
+            } while (string.IsNullOrEmpty(groupaddress));
+            do
+            {
+                WriteLine("输入端口号：");
+                var portstr = ReadLine();
+                int.TryParse(portstr, out port);
+            } while (port==0);
+            ReaderAsync(groupaddress, port).Wait();
+            ReadKey();
+        }
+
+        private static async Task ReaderAsync(string groupaddress, int port) {
+            using (var client=new UdpClient(port))
+            {
+                var readip = IPAddress.Parse(groupaddress);
+                client.JoinMulticastGroup(readip);
+                WriteLine($"加入MulticastGroup {readip}");
+                bool completed = false;
+                do
+                {
+                    WriteLine("开始接收");
+                    UdpReceiveResult result = await client.ReceiveAsync();
+                    byte[] datagram = result.Buffer;
+                    string received = Encoding.UTF8.GetString(datagram);
+                    WriteLine($"收到：{received}");
+                    if (received.ToLower() == "bye") {
+                        completed = true;
+                    }
+                } while (!completed);
+                WriteLine("接收关闭");
+                client.DropMulticastGroup(readip);
+            }
+        
+        
+        }
+    }
+}
