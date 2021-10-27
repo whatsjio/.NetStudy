757af069b7873d1bee5d1d462d235821cbc1b44c
changsheng fu <909412615@qq.com>
2021-10-27 15:46:20 +0800

    feature:接收器初步代码

--- repos/套接字接收器/Program.cs
+++ repos/套接字接收器/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace 套接字接收器
 {
@@ -10,6 +11,17 @@ namespace 套接字接收器
     {
         static void Main(string[] args)
         {
+            WriteLine("输入host");
+            var hoststr = ReadLine();
+            WriteLine("输入监听端口");
+            var portstr = ReadLine();
+            int.TryParse(portstr, out int port);
+            WriteLine("按下回车键开始");
+            ReadLine();
+
         }
+
+
+
     }
 }
