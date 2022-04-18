fded471211270cb1aa21cebf6661c8c45fe52a92
changsheng fu <909412615@qq.com>
2022-04-18 16:27:45 +0800

    feature:实例代码测试

---
+++ repos/.Net理解学习/经典实例创建解析/newinitexample/newinitexample/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace newinitexample
+{
+    class Myclass
+    {
+
+    }
+
+    struct MyStruct
+    {
+        
+    }
+
+}
---
+++ repos/.Net理解学习/经典实例创建解析/newinitexample/newinitexample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace newinitexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStruct ms = new MyStruct();
+            Myclass mc = new Myclass();
+            Console.WriteLine("IL KeyWords.");
+            Console.Read();
+        }
+    }
+}
