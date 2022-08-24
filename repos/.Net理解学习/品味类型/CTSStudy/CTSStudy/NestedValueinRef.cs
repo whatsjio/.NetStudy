d44da37ad42382b140aedb7d23478901a7e76189
changsheng fu <909412615@qq.com>
2022-08-24 11:40:53 +0800

    feature；更新代码

---
+++ repos/.Net理解学习/品味类型/CTSStudy/CTSStudy/NestedValueinRef.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTSStudy
+{
+    public class NestedValueinRef
+    {
+        private int aInt;
+
+        public NestedValueinRef()
+        {
+            char achar = 'a';
+        }
+    }
+
+    public struct NestedRefinValue
+    {
+
+
+    }
+}
